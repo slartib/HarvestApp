38eff61ef4525e5eb8d77d9f585868e19a83453d
rick <rick@rdaltonphotos.com>
2022-06-12 03:33:40 -0700

    Project Commit

---
+++ harvest-app-webui/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link, Outlet, Route, Routes, useLocation, useNavigate, useParams } from 'react-router-dom';
+import Home from './components/Home';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Home</h2>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ harvest-app-webui/src/Components/Home.jsx
@@ -0,0 +1,6 @@
+import React, {useState, useEffect} from 'react';
+import PropTypes from 'prop-types';
+import {Link, useParams} from "react-router-dom";
+import PropTypes from 'prop-types';
+
+
